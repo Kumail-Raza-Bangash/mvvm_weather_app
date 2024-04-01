d86eb0c9402855ef03a3551ab1b3a584de83bc82
Kumail-Raza-Bangash <kumailraza260@gmail.com>
2024-04-01 11:34:19 +0500

    Utils

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -9,6 +9,7 @@ class Utils {
     'null': 'assets/images/nightStatRain.png',
   };
 
+  //Code for DATE
   static String formateDate(DateTime date)
   {
     String formattedDate = DateFormat('EEEE d MMM').format(date);
@@ -26,8 +27,41 @@ class Utils {
   {
     DateTime dateTime = DateTime.parse(date);
     String formattedDate = DateFormat('EEEE').format(dateTime);
-    String abbreviatedDay = formattedDate.substring(1, 3);
+    String abbreviatedDay = formattedDate.substring(0, 3);
     return abbreviatedDay;
   }
-  
+
+  //Code for TIME
+  static String formateTime(String time)
+  {
+    DateFormat dateFormat = DateFormat('hh:mm a');
+    DateTime dateTime = DateTime.parse(time);
+    String timeIn12HourFormat = dateFormat.format(dateTime);
+    return timeIn12HourFormat; //3:30 PM
+  }
+
+  static String currentTime(String time)
+  {
+    DateFormat dateFormat = DateFormat('hh:mm a');
+    DateTime now = DateTime.now();
+    String time = dateFormat.format(now);
+    return time;
+  }
+
+  static String formateTimeWithoutAmPm(String time)
+  {
+    DateFormat dateFormat = DateFormat('hh:mm');
+    DateTime dateTime = DateTime.parse(time);
+    String timeIn24HourFormat = dateFormat.format(dateTime);
+    return timeIn24HourFormat; //15:30
+  }
+
+    static bool checkTime(String time)
+  {
+    DateFormat dateFormat = DateFormat('hh a');
+    DateTime dateTime = DateTime.parse(time);
+    DateTime now = DateTime.now();
+    return dateFormat.format(dateTime) == dateFormat.format(now);
+  }
+
 }
