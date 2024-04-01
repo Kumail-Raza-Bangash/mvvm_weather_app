c654af6d71338302a0a97bfbf9bd5eb5631f361f
Kumail-Raza-Bangash <kumailraza260@gmail.com>
2024-04-01 13:17:02 +0500

    Home Controller

---
+++ lib/view_model/controllers/days_controller.dart
@@ -0,0 +1,29 @@
+import 'package:get/get.dart';
+import 'package:mvvm_weather_app/model/data_model.dart';
+import 'package:mvvm_weather_app/resourses/images/image_assets.dart';
+import 'package:mvvm_weather_app/utils/utils.dart';
+import 'package:mvvm_weather_app/view_model/controllers/home_controller.dart';
+
+class DaysController extends GetxController {
+  Rx<int> currentDay = 0.obs;
+  final homeController = Get.put(HomeController());
+  late Rx<Days> day;
+
+  DaysController() {
+    day = homeController.model.value!.days![0].obs;
+  }
+
+  setDay(int index) {
+    day.value = homeController.model.value!.days![index];
+    currentDay.value = index;
+  }
+
+  String getImage(int index) {
+    Utils utils = Utils();
+    return utils.imageMap[
+                homeController.model.value!.days![index].conditions] ==
+            null
+        ? ImageAssets.nightStarRain
+        : Utils().imageMap[homeController.model.value!.days![index].conditions]!;
+  }
+}
