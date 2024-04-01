20ee215b5c953eae7c4d4d7f33fedb4afacde76d
Kumail-Raza-Bangash <kumailraza260@gmail.com>
2024-04-01 12:13:07 +0500

    AppExceptions

---
+++ lib/data/network/api_services.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:mvvm_weather_app/data/app_exceptions.dart';
+import 'package:mvvm_weather_app/data/network/base_api_services.dart';
+
+class ApiServices extends BaseApiServices
+{
+  dynamic jsonResponse(http.Response response)
+  {
+    switch(response.statusCode)
+    {
+      case 200:
+        var jsonResponse = jsonDecode(response.body);
+        return jsonResponse;
+
+      case 400:
+        var jsonResponse = jsonDecode(response.body);
+        return jsonResponse;
+
+      default:
+        throw FetchDataException(
+          'Error while communication ${response.statusCode}'
+        ); 
+    }
+  }
+
+
+  @override
+  Future getApi (String url) async 
+  {
+    var jsonData;
+    try
+    {
+      var response = await http.get(Uri.parse(url)).timeout(const Duration(seconds: 10));
+      jsonData = jsonResponse(response);
+    } on RequestTimeOut 
+    {
+      throw RequestTimeOut('Request TimeOut');
+    } on SocketException 
+    {
+      throw RequestTimeOut('No Internet');
+    }
+
+    return jsonData;
+  }
+
+
+
+}
