09165efbd06900be6e5d3b05cfd1e312a3c5bb72
Kumail-Raza-Bangash <kumailraza260@gmail.com>
2024-04-23 17:32:34 +0500

    Updated Code

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'MVVM Weather App',
+      title: 'MVVM Weather Apps',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
